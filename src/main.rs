a9d659470f1a3e3c44bd685a1ab3c3ced597d4e2
GregoireHENRY <gregoire.henry@ipsa.fr>
2020-05-30 10:41:08 +0200

    Init optimization branch

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
+extern crate ndarray;
 extern crate itertools;
 
-mod toolbox;
-mod math;
-mod system;
-mod body;
-mod physics;
-mod time;
-
-use math::{State};
+mod propag;
 
 fn main() {
-    let mut system = system::new("ICRS"); 
-    system.bodies.add(body::new("133P", 1e13, 2e3, State::zero(), "center"));
-    system.bodies.add(body::new("spacecraft", 10., 0.01, State::new(-5e3, 0., 0., 0., 1., 0.), "propagation"));
-    let time = time::set(0., system::DAY, 20.);
-    physics::rk(&mut system, &time);
+    let frame = "ICRS";
+    let nbody = 2;
+    let time = propag::set_time(0., propag::DAY, 20.);
+
+    let mut propag = propag::new(frame, nbody, time);
+
+    // Create every frozen before, then add the body to be propagated
+    // name, mass, radius, x, y, z, vx, vy, vz
+    propag.add("133P", 1e13, 2e3, 0., 0., 0., 0., 0. ,0.);
+    propag.propagate_nexts();
+    propag.add("spacecraft", 10., 0.01, -5e3, 0., 0., 0., 1., 0.);
+
+    propag.start();
 }
